62ace6189d5e665c29c1bdc85f35d5a767044a65
jinioh88 <jinioh88@gmail.com>
2021-01-06 22:00:11 +0900

    JDBC API 사용

--- message/src/main/java/com/study/message/AppConfig.java
+++ message/src/main/java/com/study/message/AppConfig.java
@@ -1,10 +1,24 @@
 package com.study.message;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 @ComponentScan("com.study.message")
 public class AppConfig {
 
+    @Bean
+    public FilterRegistrationBean<AuditingFilter> auditingFilterFilterRegistrationBean() {
+        FilterRegistrationBean<AuditingFilter> registration = new FilterRegistrationBean<>();
+        AuditingFilter filter = new AuditingFilter();
+        registration.setFilter(filter);
+        registration.setOrder(Integer.MAX_VALUE);
+        registration.setUrlPatterns(Arrays.asList("/message/*"));
+
+        return registration;
+    }
 }
--- message/src/main/java/com/study/message/MessageRepository.java
+++ message/src/main/java/com/study/message/MessageRepository.java
@@ -2,13 +2,55 @@ package com.study.message;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.*;
 
 @Component
 public class MessageRepository {
+
+    private DataSource dataSource;
+
+    public MessageRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     private final static Log log = LogFactory.getLog(MessageRepository.class);
 
-    public void saveMessage(Message message) {
-        log.info("Saved message!");
+    public Message saveMessage(Message message) {
+        Connection c = DataSourceUtils.getConnection(dataSource);
+        try {
+            String insertSql = "insert into messages (`id`, `text`, `created_date`) value (null, ?,?)";
+            PreparedStatement ps = c.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, message.getText());
+            ps.setTimestamp(2, new Timestamp(message.getCreatedDate().getTime()));
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected > 0) {
+                ResultSet result = ps.getGeneratedKeys();
+                if(result.next()) {
+                    int id = result.getInt(1);
+                    return new Message(id, message.getText(), message.getCreatedDate());
+                } else {
+                    log.error("failed to retrieve id.");
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            log.error("failed to save message", e);
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                log.error("fail to close", ex);
+            }
+        } finally {
+            DataSourceUtils.releaseConnection(c, dataSource);
+        }
+
+        return null;
     }
 }
