fd59d313a9d4ca7e9664ef93649a2ad360c66c7e
OH Nick <nick@herren.co.kr>
2021-01-18 17:57:56 +0900

    Test

---
+++ app/src/test/java/com/taskagile/app/RegistrationPayloadTests.java
@@ -0,0 +1,15 @@
+package com.taskagile.app;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.Validator;
+
+@SpringBootTest
+public class RegistrationPayloadTests {
+
+    private Validator validator;
+
+//    @Before
+    public void setup() {
+
+    }
+}
